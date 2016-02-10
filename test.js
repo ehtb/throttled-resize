fe01260834ce109a7df4ed6039f8d5278300ee83
Edo <i@edo.me>
2016-02-10 16:02:05 +0100

    Update tests with destroy method

--- test.js
+++ test.js
@@ -3,31 +3,60 @@
 import test from 'tape';
 import Resize from './index';
 
+const resize = new Resize();
+
 test('resize emits event', function(t) {
-  t.plan(2);
+  t.plan(1);
 
-  let resize = new Resize(),
-      i = 0;
+  let
+    called = 0,
+    i = 0;
 
-  resize.on('resize:start', () => {
-    t.pass('resize:start dispatched');
-  });
+  resize.on(
+    'resize:start', () => {
+      called++;
+    }
+  );
 
-  resize.on('resize:end', () => {
-    t.pass('resize:end dispatched');
-  });
+  resize.on(
+    'resize:end', () => {
+      called++;
+    }
+  );
 
   setTimeout(function tick() {
-    if (++i < 20) {
-      if (document.createEvent) {
-        var ev = document.createEvent('Event');
-        ev.initEvent('resize', true, true);
-        window.dispatchEvent(ev);
-      } else {
-        document.fireEvent('onresize');
-      }
-
+    if (++i < 10) {
+      fire();
       setTimeout(tick, 10);
+    } else {
+      setTimeout(() => {
+        t.equal(called, 2);
+        t.end();
+      }, 500);
     }
   }, 10);
 });
+
+test('destroy', function(t) {
+  t.plan(0);
+
+  resize.on(
+    'resize:end', () => t.fail('resize:end wrongly dispatched')
+  );
+
+  resize.destroy();
+
+  fire();
+
+  t.end();
+});
+
+function fire() {
+  if (document.createEvent) {
+    var ev = document.createEvent('Event');
+    ev.initEvent('resize', true, true);
+    window.dispatchEvent(ev);
+  } else {
+    document.fireEvent('onresize');
+  }
+}
